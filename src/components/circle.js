c4d9bd7101a632a5a3bc51ec7d14ed0f6bcbbcad
Shad Azam <58129443+shadazam08@users.noreply.github.com>
2023-01-06 21:39:06 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,21 @@
 
 
 // export class using module.exports
+
+/ export class using module.exports
+module.exports = Shape;
+
+// import using require
+const Shape = require('./shape');
+
+// declare class
+
+class Circle extends Shape {
+    calculateArea(radius) {
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
+
+// export class using module.exports
+
+module.exports = Circle;
